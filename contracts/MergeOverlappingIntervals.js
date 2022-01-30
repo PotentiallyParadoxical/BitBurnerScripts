faf249dc0855412a37c8b528932131972f4c0fa8
pdx <fluffyandy97@gmail.com>
2022-01-29 22:54:38 -0800

    Refactored contracter and how server crawling is handled due to a bug I noticed. I will switch to using typescript in the future because I want to put all the contract functions in a namespace.

--- contracts/MergeOverlappingIntervals.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-	ns.tprint(MergeOverLappingIntervals(ns.args[0]));
-}
-export function MergeOverLappingIntervals(n) {
-	
-	return i;
-}
--- contracts/genIpAddr.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-	ns.tprint(GenerateIPAddresses(ns.args[0]));
-}
-export function GenerateIPAddresses(n) {
-	
-	return i;
-}
